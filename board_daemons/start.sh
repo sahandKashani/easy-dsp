914abada051bf961890dc13a200ca61fdee6bd7d
Sepand KASHANI <kashani.sepand@gmail.com>
2017-03-05 21:14:49 +0100

    Make logs.txt the default log file.

--- board_daemons/start.sh
+++ board_daemons/start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-if [ "${#}" -eq 0 ]; then
-    echo 'Error: ${1} must be the name of a log file.' 2>&1
-    exit 1
-fi
-
-log_file="${1}"
-
 ./stop.sh > /dev/null
+
+log_file=logs.txt
+echo > "${log_file}"
 
 echo -n 'Starting daemons '
 nohup ./browser-main-daemon >> "${log_file}" 2>&1 &
